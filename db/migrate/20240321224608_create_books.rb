c20386a7dadab220aca4897c9e8997854c72327a
sajan <sajanghuman18@gmail.com>
2024-03-21 17:46:45 -0500

    Add active admin for admin dashboard

---
+++ db/migrate/20240321224608_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :books do |t|
+      t.string :book_name
+      t.string :book_author
+      t.string :book_publisher
+      t.string :book_genre
+      t.string :book_image
+
+      t.timestamps
+    end
+  end
+end
